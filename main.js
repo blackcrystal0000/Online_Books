7c403728f30617ad09c96d58f85e0c5e1e0da18c
blackcrystal0000 <125825960+blackcrystal0000@users.noreply.github.com>
2023-07-06 04:30:55 -0700

    Added the fetchBooks function

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ document.getElementById('search-input').addEventListener('keypress', function(e)
         fetchBooks(searchText);
     }
 });
+
+function fetchBooks(searchText) {
+    fetch(`https://openlibrary.org/search.json?title=${searchText}`)
+        .then(response => response.json())
+        .then(data => {
+            let booksList = '';
+            if(data.docs){
+                data.docs.forEach(function(book) {
+                    let title = book.title ? book.title : "No Title";
+                    let author = book.author_name ? book.author_name.join(', ') : "Unknown Author";
+                    booksList += `
+                        <div class="book">
+                            <h2>${title}</h2>
+                            <h3>${author}</h3>
+                            <p class="like-btn" onclick="likeBook(event)">❤ Like</p>
+                        </div>
+                    `;
+                });
+            } else {
+                booksList = "<p>No results found</p>";
+            }
+        }
+    )}
