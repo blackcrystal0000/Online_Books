e90229dce7df6639b1693cab8651259b23946ed6
blackcrystal0000 <125825960+blackcrystal0000@users.noreply.github.com>
2023-07-06 03:00:43 -0700

    Add the event listener click

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     fetchBooks('');
 })
+
+document.getElementById('search-btn').addEventListener('click', function(){
+    let searchText = document.getElementById('search-input').value;
+    fetchBooks(searchText);
+});
