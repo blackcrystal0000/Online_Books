e893df77f1685d122c28b9c9fddc5127b88fd911
blackcrystal0000 <125825960+blackcrystal0000@users.noreply.github.com>
2023-07-06 03:10:38 -0700

    Added SearchText

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ document.getElementById('search-btn').addEventListener('click', function(){
     let searchText = document.getElementById('search-input').value;
     fetchBooks(searchText);
 });
+
+document.getElementById('search-input').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        let searchText = document.getElementById('search-input').value;
+        fetchBooks(searchText);
+    }
+});
